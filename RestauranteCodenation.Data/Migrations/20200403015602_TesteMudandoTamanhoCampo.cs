d34d566f57e62f5302d72db7f97820aaf9394ae1
Afonso Carvalho <afonsohenriquesc@hotmail.com>
2020-04-04 18:47:33 -0300

    Adicinando Codigo no Git

---
+++ RestauranteCodenation.Data/Migrations/20200403015602_TesteMudandoTamanhoCampo.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestauranteCodenation.Data.Migrations
+{
+    public partial class TesteMudandoTamanhoCampo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Ingrediente",
+                type: "varchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Ingrediente",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(500)",
+                oldNullable: true);
+        }
+    }
+}
